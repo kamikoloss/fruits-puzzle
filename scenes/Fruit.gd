89b3b30c1040e9f0cc93d09e009fde1c9bbe6ebf
Kamiko Loss <info@gloxi.net>
2023-10-03 22:39:21 +0900

    落とすフルーツの色が変わるようにした

--- scenes/Fruit.gd
+++ scenes/Fruit.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Member
 var _id = 0
 var _type = Global.FruitType.NONE
+var _data = null # FRUIT_DATA
 
 
 # 初期化
@@ -11,18 +12,19 @@ var _type = Global.FruitType.NONE
 func initialize(id, type):
 	_id = id
 	_type = type
+	_data = Global.FRUIT_DATA[_type]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_apply_scale()
 	_apply_color()
-	print("Fruit is created. (id: {id}, type: {type})".format({"id": _id, "type": Global.FRUIT_DATA[_type]["name"]}))
+	print("Fruit is created. (id: {id}, type: {type})".format({"id": _id, "type": _data["name"]}))
 
 
-# 種類を元に自身のスケールを決定する
+# 種類を元に自身の大きさを決定する
 func _apply_scale():
-	var _new_scale = float(Global.FRUIT_DATA[_type]["scale"]) / 256
+	var _new_scale = float(_data["scale"]) / 256
 	scale = Vector2(_new_scale, _new_scale)
 	
 	# 自身のスケールを子ノードに適用する (最後に戻す)
@@ -34,4 +36,4 @@ func _apply_scale():
 
 # 種類を元に自身の色を決定する
 func _apply_color():
-	pass
+	get_node("RigidBody2D/Circle").modulate = Color(_data["color"])
--- scenes/Global.gd
+++ scenes/Global.gd
@@ -14,17 +14,17 @@ enum FruitType {
 	WATERMELON,
 }
 
-# フルーツのデータ (名前, 点数, 大きさ)
+# フルーツのデータ (名前, 点数, 大きさ, 色)
 const FRUIT_DATA = {
-	FruitType.CHERRY: { "name": "CHERRY", "score": 1, "scale": 16 },
-	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 32 },
-	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 48 },
-	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 64 },
-	FruitType.PERSIMMON: { "name": "PERSIMMON", "score": 16, "scale": 80 },
-	FruitType.APPLE: { "name": "APPLE", "score": 32, "scale": 96 },
-	FruitType.PEAR: { "name": "PEAR", "score": 64, "scale": 112 },
-	FruitType.PEACH: { "name": "PEACH", "score": 128, "scale": 128 },
-	FruitType.PINEAPPLE: { "name": "PINEAPPLE", "score": 256, "scale": 144 },
-	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 160 },
-	FruitType.WATERMELON: { "name": "WATERMELON", "score": 1024, "scale": 176 },
+	FruitType.CHERRY: { "name": "CHERRY", "score": 1, "scale": 16, "color": "#8b0000" },
+	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 32, "color": "#b22222" },
+	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 48, "color": "#9370db" },
+	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 64, "color": "#f4a460" },
+	FruitType.PERSIMMON: { "name": "PERSIMMON", "score": 16, "scale": 80, "color": "#ff8c00" },
+	FruitType.APPLE: { "name": "APPLE", "score": 32, "scale": 96, "color": "#dc143c" },
+	FruitType.PEAR: { "name": "PEAR", "score": 64, "scale": 112, "color": "#f0e68c" },
+	FruitType.PEACH: { "name": "PEACH", "score": 128, "scale": 128, "color": "#ffb6c1" },
+	FruitType.PINEAPPLE: { "name": "PINEAPPLE", "score": 256, "scale": 144, "color": "#ffd700" },
+	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 160, "color": "#9acd32" },
+	FruitType.WATERMELON: { "name": "WATERMELON", "score": 1024, "scale": 176, "color": "#32cd32" },
 }
