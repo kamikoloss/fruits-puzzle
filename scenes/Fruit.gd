a35e6d58dcd3a9d765d4f465301746c27a84f5d0
Kamiko Loss <info@gloxi.net>
2023-10-03 22:23:28 +0900

    落とすフルーツのサイズがランダムで変わるようにした

--- scenes/Fruit.gd
+++ scenes/Fruit.gd
@@ -1,19 +1,37 @@
 extends Node2D
 
+
 # Member
-var id = 0
-var type = 0
-var score = 0
+var _id = 0
+var _type = Global.FruitType.NONE
+
+
+# 初期化
+# add_child() より前に呼ぶこと
+func initialize(id, type):
+	_id = id
+	_type = type
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_scale_to_children()
+	_apply_scale()
+	_apply_color()
+	print("Fruit is created. (id: {id}, type: {type})".format({"id": _id, "type": Global.FRUIT_DATA[_type]["name"]}))
 
 
-# 自身のスケールを子ノードに適用する
-# ref. https://github.com/godotengine/godot/issues/5734
-func apply_scale_to_children():
+# 種類を元に自身のスケールを決定する
+func _apply_scale():
+	var _new_scale = float(Global.FRUIT_DATA[_type]["scale"]) / 256
+	scale = Vector2(_new_scale, _new_scale)
+	
+	# 自身のスケールを子ノードに適用する (最後に戻す)
+	# ref. https://github.com/godotengine/godot/issues/5734
 	get_node("RigidBody2D/Circle").scale *= scale
 	get_node("RigidBody2D/CollisionShape2D").scale *= scale
 	scale = Vector2.ONE
+
+
+# 種類を元に自身の色を決定する
+func _apply_color():
+	pass
--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,79 +1,100 @@
 extends Node2D
 
 
-# Fruit
-var FRUIT_SCENE = preload("res://scenes/Fruit.tscn")
-var current_fruit = null
-var next_fruit = null
+const FRUIT_SCENE = preload("res://scenes/Fruit.tscn")
 const FRUIT_DEFAULT_POSITION = Vector2i(180, 160)
+const FRUIT_DEFAULT_TYPES = [
+	Global.FruitType.CHERRY,
+	Global.FruitType.STRAWBERRY,
+	Global.FruitType.GRAPE,
+	Global.FruitType.DEKOPON,
+	Global.FruitType.PERSIMMON,
+]
 const FRUIT_MOVE_SPEED = 1 # px/frame
 const FRUIT_DELAY_SECOND = 1
+
+# Game
+var _state = 0
+var _score = 0
+
+# Fruit
+var _current_fruit = null
+var _current_fruit_id = 0
+var _next_fruit = null
+
 # Button
-var is_button_left_down = false
-var is_button_right_down = false
-# Score
-var score = 0
+var _is_button_left_down = false
+var _is_button_right_down = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	create_new_fruit()
+	_create_new_fruit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_fruit()
-
-
-# 新しいフルーツを生成する
-# TODO: 引数でフルーツの種類を指定できるようにする
-func create_new_fruit():
-	current_fruit = FRUIT_SCENE.instantiate()
-	current_fruit.global_position = FRUIT_DEFAULT_POSITION
-	current_fruit.scale = Vector2(0.125, 0.125)
-	current_fruit.get_node("RigidBody2D").freeze = true
-	add_child(current_fruit)
-
-
-# フルーツを左右に動かす
-func move_fruit():
-	var offset = Vector2(FRUIT_MOVE_SPEED, 0)
-	if is_button_left_down:
-		current_fruit.global_translate(offset * -1)
-	if is_button_right_down:
-		current_fruit.global_translate(offset)
-
-
-# フルーツを落とす
-func drop_fruit():
-	current_fruit.get_node("RigidBody2D").freeze = false
-	current_fruit = null
+	_move_fruit()
 
 
 # Events
 func _on_button_left_button_down():
-	is_button_left_down = true
+	_is_button_left_down = true
 
 
 func _on_button_left_button_up():
-	is_button_left_down = false
+	_is_button_left_down = false
 
 
 func _on_button_right_button_down():
-	is_button_right_down = true
+	_is_button_right_down = true
 
 
 func _on_button_right_button_up():
-	is_button_right_down = false
+	_is_button_right_down = false
 
 
 func _on_button_drop_button_up():
-	if current_fruit == null:
+	if _current_fruit == null:
 		return
-
+	
 	# 現在のフルーツを落とす
-	drop_fruit()
+	_drop_fruit()
 	# フルーツが落ちるまで少し待つ
+	# TODO: フルーツが落ちたタイミングを判定する
 	await get_tree().create_timer(FRUIT_DELAY_SECOND).timeout
 	# 次のフルーツを作成する
-	create_new_fruit()
+	_create_new_fruit()
+
+
+# 新しいフルーツを生成する
+# TODO: 引数でフルーツの種類を指定できるようにする
+func _create_new_fruit():
+	_current_fruit = FRUIT_SCENE.instantiate()
+	
+	var _new_type = FRUIT_DEFAULT_TYPES[randi() % FRUIT_DEFAULT_TYPES.size()]
+	_current_fruit.initialize(_current_fruit_id,_new_type )
+	_current_fruit_id += 1
+	
+	_current_fruit.global_position = FRUIT_DEFAULT_POSITION
+	_current_fruit.get_node("RigidBody2D").freeze = true
+	
+	add_child(_current_fruit)
+
+
+# フルーツを左右に動かす
+func _move_fruit():
+	if _current_fruit == null:
+		return
+	
+	var _offset = Vector2(FRUIT_MOVE_SPEED, 0)
+	if _is_button_left_down:
+		_current_fruit.global_translate(_offset * -1)
+	if _is_button_right_down:
+		_current_fruit.global_translate(_offset)
+
+
+# フルーツを落とす
+func _drop_fruit():
+	_current_fruit.get_node("RigidBody2D").freeze = false
+	_current_fruit = null
