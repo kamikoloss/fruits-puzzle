86b5f4dacc8410d0e6602c9838eb9ad73daf922c
Kamiko Loss <info@gloxi.net>
2023-10-05 20:26:06 +0900

    次のフルーツを表示するようにした

--- scenes/game.gd
+++ scenes/game.gd
@@ -16,12 +16,13 @@ const FRUIT_DELAY_SECOND = 1.0
 
 # 現在/次のフルーツ
 var _current_fruit = null
-var _next_fruit = null
 var _current_fruit_id = 0
+var _next_fruit_type = Global.FruitType.NONE
+var _next_fruit_sprite = null
 
 # UI
-var _text_score = null
-var _text_next = null
+var _score_text = null
+var _next_text = null
 
 # ボタンの状態
 var _is_button_left_down = false
@@ -34,10 +35,12 @@ func _ready():
 	Global.score_changed.connect(_on_score_changed)
 	
 	# UI
-	_text_score = $"../UI/CanvasLayer/VBoxContainer/Header/MarginContainer/Scores/TextScore"
-	_text_next = $"../UI/CanvasLayer/VBoxContainer/Header/MarginContainer/Scores/TextNext"
-	_text_score.text = "SCORE: {0}".format([0])
+	_score_text = $"../UI/CanvasLayer/VBoxContainer/Header/MarginContainer/Texts/Score"
+	_next_text = $"../UI/CanvasLayer/VBoxContainer/Header/MarginContainer/Texts/Next"
+	_next_fruit_sprite = $"../UI/CanvasLayer/VBoxContainer/Header/NextFruit"
+	_score_text.text = "SCORE: {0}".format([0])
 	
+	_choose_next_fruit()
 	_create_new_fruit()
 
 
@@ -46,23 +49,23 @@ func _process(delta):
 	_move_fruit()
 
 
-func _on_button_left_button_down():
+func _on_left_button_down():
 	_is_button_left_down = true
 
 
-func _on_button_left_button_up():
+func _on_left_button_up():
 	_is_button_left_down = false
 
 
-func _on_button_right_button_down():
+func _on_right_button_down():
 	_is_button_right_down = true
 
 
-func _on_button_right_button_up():
+func _on_right_button_up():
 	_is_button_right_down = false
 
 
-func _on_button_drop_button_up():
+func _on_drop_button_up():
 	if _current_fruit == null:
 		return
 	
@@ -79,22 +82,35 @@ func _on_button_drop_button_up():
 
 # スコア変更時の処理
 func _on_score_changed(score):
-	_text_score.text = "SCORE: {0}".format([score])
+	_score_text.text = "SCORE: {0}".format([score])
 
 
-# 新しいフルーツを生成する
-# TODO: 引数でフルーツの種類を指定できるようにする
+# 新しいフルーツを作成する
 func _create_new_fruit():
 	_current_fruit = FRUIT_SCENE.instantiate()
-
-	var _new_type = FRUIT_DEFAULT_TYPES[randi() % FRUIT_DEFAULT_TYPES.size()]
-	#_text_next.text = "NEXT: {0}.format(_new_type)"
-	
-	_current_fruit.setup(_current_fruit_id, _new_type)
+	_current_fruit.setup(_current_fruit_id, _next_fruit_type)
 	_current_fruit_id += 1
 	_current_fruit.global_position = FRUIT_DEFAULT_POSITION
 	_current_fruit.get_node("RigidBody2D").freeze = true
 	get_tree().root.get_node("Main/Fruits").add_child(_current_fruit)
+	
+	_choose_next_fruit()
+
+
+# 次のフルーツを選択する
+func _choose_next_fruit():
+	# 次のフルーツの種類を抽選する
+	_next_fruit_type = FRUIT_DEFAULT_TYPES[randi() % FRUIT_DEFAULT_TYPES.size()]
+	var _next_fruit_data = Global.FRUIT_DATA[_next_fruit_type]
+	
+	# 次のフルーツの文字を更新する
+	var _next_fruit_name = _next_fruit_data["name"]
+	_next_text.text = "NEXT: {0}".format([_next_fruit_name])
+	
+	# 次のフルーツの画像を更新する
+	var _next_scale = float(_next_fruit_data["scale"]) / 256
+	_next_fruit_sprite.scale = Vector2(_next_scale, _next_scale)
+	_next_fruit_sprite.modulate = Color(_next_fruit_data["color"])
 
 
 # フルーツを左右に動かす
