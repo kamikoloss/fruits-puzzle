611498f9194734628c7ea10becf38ad94de05b62
Kamiko Loss <info@gloxi.net>
2023-10-05 20:26:22 +0900

    フルーツの大きさを調整した

--- scenes/global.gd
+++ scenes/global.gd
@@ -26,16 +26,16 @@ enum FruitType {
 # ref. https://www.colordic.org/y
 const FRUIT_DATA = {
 	FruitType.CHERRY: { "name": "CHERRY", "score": 1, "scale": 16, "color": "#942343" },
-	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 32, "color": "#d70035" },
-	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 48, "color": "#56256e" },
-	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 64, "color": "#f6ae54" },
-	FruitType.PERSIMMON: { "name": "PERSIMMON", "score": 16, "scale": 80, "color": "#f3981d" },
-	FruitType.APPLE: { "name": "APPLE", "score": 32, "scale": 96, "color": "#d70035" },
-	FruitType.PEAR: { "name": "PEAR", "score": 64, "scale": 112, "color": "#fad09e" },
-	FruitType.PEACH: { "name": "PEACH", "score": 128, "scale": 128, "color": "#f19ca7" },
-	FruitType.PINEAPPLE: { "name": "PINEAPPLE", "score": 256, "scale": 144, "color": "#fcc800" },
-	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 160, "color": "#65ab31" },
-	FruitType.WATERMELON: { "name": "WATERMELON", "score": 1024, "scale": 176, "color": "#417038" },
+	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 24, "color": "#d70035" },
+	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 32, "color": "#56256e" },
+	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 40, "color": "#f6ae54" },
+	FruitType.PERSIMMON: { "name": "PERSIMMON", "score": 16, "scale": 48, "color": "#f3981d" },
+	FruitType.APPLE: { "name": "APPLE", "score": 32, "scale": 56, "color": "#d70035" },
+	FruitType.PEAR: { "name": "PEAR", "score": 64, "scale": 64, "color": "#fad09e" },
+	FruitType.PEACH: { "name": "PEACH", "score": 128, "scale": 80, "color": "#f19ca7" },
+	FruitType.PINEAPPLE: { "name": "PINEAPPLE", "score": 256, "scale": 96, "color": "#fcc800" },
+	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 112, "color": "#65ab31" },
+	FruitType.WATERMELON: { "name": "WATERMELON", "score": 1024, "scale": 128, "color": "#417038" },
 }
 
 
