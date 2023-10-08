ee75a24fc57991561429c1da4aaebc08a1698001
Kamiko Loss <info@gloxi.net>
2023-10-08 15:31:19 +0900

    フルーツの大きさの基準と色を変更した

--- scenes/global.gd
+++ scenes/global.gd
@@ -25,21 +25,21 @@ enum FruitType {
 
 # フルーツの大きさの基準
 # 256 * FRUIT_DATA["scale"] / FRUIT_SIZE_BASE (px) になる
-const FRUIT_SIZE_BASE = 200
+const FRUIT_SIZE_BASE = 224
 
 # フルーツのデータ (名前, 点数, 大きさ, 色)
 # ref. https://www.colordic.org/y
 const FRUIT_DATA = {
-	FruitType.CHERRY: { "name": "CHERRY", "score": 1, "scale": 16, "color": "#942343" },
-	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 24, "color": "#d70035" },
-	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 32, "color": "#56256e" },
-	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 40, "color": "#f6ae54" },
+	FruitType.CHERRY: { "name": "CHERRY", "score": 1, "scale": 16, "color": "#b33e5c" },
+	FruitType.STRAWBERRY: { "name": "STRAWBERRY", "score": 2, "scale": 24, "color": "#e73562" },
+	FruitType.GRAPE: { "name": "GRAPE", "score": 4, "scale": 32, "color": "#915da3" },
+	FruitType.DEKOPON: { "name": "DEKOPON", "score": 8, "scale": 40, "color": "#fac559" },
 	FruitType.PERSIMMON: { "name": "PERSIMMON", "score": 16, "scale": 48, "color": "#f3981d" },
 	FruitType.APPLE: { "name": "APPLE", "score": 32, "scale": 56, "color": "#d70035" },
 	FruitType.PEAR: { "name": "PEAR", "score": 64, "scale": 64, "color": "#fad09e" },
 	FruitType.PEACH: { "name": "PEACH", "score": 128, "scale": 80, "color": "#f19ca7" },
 	FruitType.PINEAPPLE: { "name": "PINEAPPLE", "score": 256, "scale": 96, "color": "#fcc800" },
-	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 112, "color": "#65ab31" },
+	FruitType.MELON: { "name": "MELON", "score": 512, "scale": 112, "color": "#9fc24d" },
 	FruitType.WATERMELON: { "name": "WATERMELON", "score": 1024, "scale": 128, "color": "#417038" },
 }
 
